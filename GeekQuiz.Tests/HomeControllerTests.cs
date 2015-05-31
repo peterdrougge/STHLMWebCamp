cfea829dcd237b299da4620b5f03e1812eab650c
Peter Drougge <peter.drougge@gmail.com>
2015-05-31 07:53:51 +0200

    initial commit

---
+++ GeekQuiz.Tests/HomeControllerTests.cs
@@ -0,0 +1,43 @@
+﻿// ----------------------------------------------------------------------------------
+// Microsoft Developer & Platform Evangelism
+// 
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// 
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+// EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES 
+// OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+// ----------------------------------------------------------------------------------
+// The example companies, organizations, products, domain names,
+// e-mail addresses, logos, people, places, and events depicted
+// herein are fictitious.  No association with any real company,
+// organization, product, domain name, email address, logo, person,
+// places, or events is intended or should be inferred.
+// ----------------------------------------------------------------------------------
+
+namespace GeekQuiz.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void Index()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void About()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void Contact()
+        {
+            // Introduce Failing Test
+            //Assert.IsFalse(true);
+        }
+    }
+}
